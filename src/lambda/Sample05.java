625f922a35bd852e68bdac367224bb131c8292fd
ones1kk <kwsic@naver.com>
2021-11-23 00:28:50 +0900

    ✨ Add and Update : Sample06 & 05

--- src/lambda/Sample05.java
+++ src/lambda/Sample05.java
@@ -18,6 +18,14 @@ public class Sample05 {
             FirstName = firstName;
         }
 
+        public SampleUser() {
+
+        }
+
+        public SampleUser(String lastName) {
+            this.lastName = lastName;
+        }
+
         public String getLastName() {
             return lastName;
         }
@@ -39,16 +47,16 @@ public class Sample05 {
 
         List<SampleUser> users = new ArrayList<>();
 
-        SampleUser user1 = new SampleUser("홍","길동");
-        SampleUser user2 = new SampleUser("김","유신");
-        SampleUser user3 = new SampleUser("","예");
+        SampleUser user1 = new SampleUser("홍", "길동");
+        SampleUser user2 = new SampleUser("김", "유신");
+        SampleUser user3 = new SampleUser("", "예");
 
         users.add(user1);
         users.add(user2);
         users.add(user3);
 
         users.removeIf(user -> user.getLastName().equals("홍"));
-        users.forEach(user-> System.out.println(user.getFirstName()+user.getLastName()));
+        users.forEach(user -> System.out.println(user.getFirstName() + user.getLastName()));
 
         ArrayList<Integer> list = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
---
+++ src/lambda/Sample06.java
@@ -0,0 +1,32 @@
+package lambda;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import lambda.Sample05.SampleUser;
+
+public class Sample06 {
+
+    // 메서드 참조(Method reference)
+    // 클래스이름::메서드 이름
+    public static void main(String[] args) {
+        Function<String, Integer> f = Integer::parseInt;
+
+        BiFunction<String, String, SampleUser> f2 = SampleUser::new;
+
+        Supplier<SampleUser> s = SampleUser::new;
+        System.out.println(s);
+        System.out.println(s.get());
+
+        Function<String, SampleUser> f1 = SampleUser::new;
+        System.out.println(f1.apply("kim").getLastName());
+
+        Function<Integer, int[]> f3 = int[]::new;
+        int [] arr = f3.apply(6);
+
+        System.out.println(arr.length);
+
+
+    }
+
+}
