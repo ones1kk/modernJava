e856bc506fb0a2e3edfdf33d3da6e585dbebf3e2
ones1kk <kwsic@naver.com>
2022-01-07 00:07:28 +0900

    ✨ Add : Thread I/O blocking

---
+++ src/thread/Sample03.java
@@ -0,0 +1,21 @@
+package thread;
+
+import javax.swing.JOptionPane;
+
+public class Sample03 {
+
+	public static void main(String[] args) {
+		// 싱글 쓰레드
+		String input = JOptionPane.showInputDialog("아무 값이나 입력하세요."); // blocking 발생
+		System.out.println("입력하신 값은 " + input + "입니다.");
+
+		for (int i = 10; i > 0; i--) { // 지연
+			System.out.println(i);
+			try {
+				Thread.sleep(1000);  // 1초간 시간을 지연한다.
+			} catch (Exception ignored) {
+			}
+		}
+	}
+
+}
