f00de91ea7fceb0ee829a273abeb134e0d989edc
ones1kk <kwsic@naver.com>
2022-01-08 00:47:40 +0900

    ✨ Add : Thread Priority

---
+++ src/thread/Sample05.java
@@ -0,0 +1,40 @@
+package thread;
+
+public class Sample05 {
+
+	public static void main(String[] args) {
+		Thread1 th1 = new Thread1();
+		Thread2 th2 = new Thread2();
+
+		th2.setPriority(7);
+
+		System.out.println("Priority of th1(-) : " + th1.getPriority());
+		System.out.println("Priority of th2(|) : " + th2.getPriority());
+		th1.start();
+		th2.start();
+	}
+
+	static class Thread1 extends Thread {
+
+		public void run() {
+			for (int i = 0; i < 300; i++) {
+				System.out.print("-");
+				for (int x = 0; x < 10000000; x++)
+					;
+			}
+		}
+	}
+
+	static class Thread2 extends Thread {
+
+		public void run() {
+			for (int i = 0; i < 300; i++) {
+				System.out.print("|");
+				for (int x = 0; x < 10000000; x++)
+					;
+			}
+		}
+	}
+}
+
+
