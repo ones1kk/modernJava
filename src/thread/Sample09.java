cc79a0a348a2012c05b1f44511e85af2afa3445f
ones1kk <kwsic@naver.com>
2022-01-11 00:32:03 +0900

    ✨ Add : Thread join

--- src/thread/Sample09.java
+++ src/thread/Sample09.java
@@ -3,10 +3,9 @@ package thread;
 public class Sample09 {
 
     public static void main(String[] args) {
-        RunImpl r = new RunImpl();
-        Thread th1 = new Thread(r, "*");
-        Thread th2 = new Thread(r, "**");
-        Thread th3 = new Thread(r, "***");
+        RunImpl th1 = new RunImpl("*");
+        RunImpl th2 = new RunImpl("**");
+        RunImpl th3 = new RunImpl("***");
         th1.start();
         th2.start();
         th3.start();
@@ -27,12 +26,42 @@ public class Sample09 {
     } // main
 
     static class RunImpl implements Runnable {
+
+        volatile boolean suspended = false; // volatile  : 쉽게 바뀌는 변수
+
+        volatile boolean stopped = false;
+
+        Thread th;
+
+        public RunImpl(String name) {
+            th = new Thread(this, name); // Thread(Runnable r, String name)
+        }
+
+        void start() {
+            th.start();
+        }
+
+        void stop() {
+            stopped = true;
+        }
+
+        void suspend() {
+            suspended = true;
+        }
+
+        void resume() {
+            suspended = false;
+        }
+
         public void run() {
-            while (true) {
-                System.out.println(Thread.currentThread().getName());
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {}
+            while (!stopped) {
+                if (!suspended) {
+                    System.out.println(Thread.currentThread().getName());
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ignored) {}
+                }
+
             }
         } // run()
     }
