0e9ed91fb0a2ed24c7c0fc14c8c52d0e2eac1207
ones1kk <kwsic@naver.com>
2022-01-24 23:53:42 +0900

    🎨 Improve : Try to improve

--- src/generic/Sample05.java
+++ src/generic/Sample05.java
@@ -3,15 +3,44 @@ package generic;
 public class Sample05 {
 
     public static void main(String[] args) {
-
+        Book book = new Book();
+        Tank.boom(book, Model.class).move();
+        Tank.boom(book).move();
     }
 
     static class Tank {
 
-        private Tank() {}
+        private Tank() {
+        }
 
-        public static <B extends Book, T> BallBox<B, T> boom(Book book, Class<T> target) {
+        public static <B extends Book, T> BallBox<B, T> boom(B book, Class<T> target) {
             return new BallBox<>(book, target);
+        }
+
+        public static <B extends Book, T> HomeBox<B , T> boom(B book) {
+            return new HomeBox<>(book);
+        }
+    }
+
+    static class Model {
+        private String aa;
+
+        private String bb;
+
+        public String getAa() {
+            return aa;
+        }
+
+        public void setAa(String aa) {
+            this.aa = aa;
+        }
+
+        public String getBb() {
+            return bb;
+        }
+
+        public void setBb(String bb) {
+            this.bb = bb;
         }
     }
 
@@ -20,12 +49,15 @@ public class Sample05 {
     }
 
     static interface InterfaceBox {
+
         InterfaceBox move();
     }
 
-    static abstract class AbstractBox<B extends Book, T> implements InterfaceBox{
+    static abstract class AbstractBox<B extends Book, T> implements InterfaceBox {
 
         protected final B book;
+
+        protected Class<T> target;
 
         protected AbstractBox(B book) {
             this.book = book;
@@ -34,15 +66,28 @@ public class Sample05 {
 
     static class BallBox<B extends Book, T> extends AbstractBox<B, T> {
 
-        private final Class<T> target;
-
         public BallBox(B book, Class<T> target) {
             super(book);
-            this.target = target;
+            super.target = target;
         }
 
         @Override
         public InterfaceBox move() {
+            System.out.println("BallBox.move");
+            System.out.println("target = " + target);
+            return this;
+        }
+    }
+
+    static class HomeBox<B extends Book, T> extends AbstractBox<B, T> {
+
+        protected HomeBox(B book) {
+            super(book);
+        }
+
+        @Override
+        public InterfaceBox move() {
+            System.out.println("HomeBox.move");
             return this;
         }
     }
