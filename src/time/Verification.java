52158b618f0eebb7ec0305b72218bd71d4afc299
ones1kk <kwsic@naver.com>
2022-03-21 23:28:53 +0900

    ✨ Add : Verification

---
+++ src/time/Verification.java
@@ -0,0 +1,67 @@
+package time;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Verification {
+
+    private final String code;
+
+    private final Duration duration;
+
+    private final LocalDateTime createdDateTime = LocalDateTime.now();
+
+
+
+    public Verification(String code, Duration duration) {
+        if(code.length() > 6) throw new RuntimeException();
+
+        if(duration.getSeconds() > 180) throw new RuntimeException();
+
+        if(code.isBlank() || code.isEmpty()) throw new RuntimeException();
+
+        this.code = code;
+        this.duration = duration;
+    }
+
+    public boolean verify(String code) {
+        if (!Objects.equals(this.code, code)) {
+            return false;
+        }
+
+        return !isExpired();
+    }
+
+    public boolean isExpired() {
+        Duration duration = Duration.between(this.createdDateTime, LocalDateTime.now());
+        return duration.isNegative() || duration.getSeconds() > this.duration.getSeconds();
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+    public LocalDateTime getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String code = "123456";
+
+        Verification verification = new Verification(code, Duration.ofMillis(1000L));
+        System.out.println(verification.verify("123456"));
+
+        Thread.sleep(2000L);
+        System.out.println(verification.verify("123123"));
+
+
+        Verification exception = new Verification("", Duration.ofMillis(100));
+
+
+    }
+}
