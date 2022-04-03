1572c34731c7e5a64a2deb5d55d3969ad8423c85
ones1kk <kwsic@naver.com>
2022-04-03 20:23:55 +0900

    ✨ Add : Self study

---
+++ src/doodle/Sample02.java
@@ -0,0 +1,125 @@
+package doodle;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Sample02{
+
+    private final Set<Model> set;
+    private final Map<String, Function<Model, String>[]> map = new HashMap<>();
+
+    public List<String> list;
+
+    public Sample02(Set<Model> set) {
+        this.set = set;
+    }
+
+    public Sample02 add(String name, Function<Model, String> function, Function<Model, String> function2) {
+        map.put(name, new Function[]{function, function2});
+        foreach();
+        return this;
+    }
+
+    private void foreach() {
+        map.forEach((k,v) -> {
+            Function<Model, String> keyFunc = v[0];
+
+            List<String> lists = this.set.stream().map(Model::getName).collect(Collectors.toList());
+            this.list = lists;
+        });
+    }
+
+    public static void main(String[] args) {
+
+        Set<Model> set = new HashSet<>();
+
+        set.add(new Model("test1", "test", 10, Grade.F));
+        set.add(new Model("test2", "test", 10, Grade.F));
+        set.add(new Model("test3", "test", 10, Grade.F));
+        set.add(new Model("test4", "test", 10, Grade.F));
+        set.add(new Model("test5", "test", 10, Grade.F));
+
+        Sample02 sample02 = new Sample02(set);
+
+        sample02.add("first", Model::getId, Model::getName);
+
+        Map<String, Function<Model, String>[]> map = sample02.map;
+        List<String> list = sample02.list;
+        System.out.println("list = " + list);
+        System.out.println("map = " + map);
+
+    }
+
+    static class Model {
+
+        private String id;
+
+        private String name;
+
+        private Integer age;
+
+        private Grade grade;
+
+        public Model(String id, String name, Integer age, Grade grade) {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.grade = grade;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        public Grade getGrade() {
+            return grade;
+        }
+
+        public void setGrade(Grade grade) {
+            this.grade = grade;
+        }
+
+        @Override
+        public String toString() {
+            return "Model{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", grade=" + grade +
+                '}';
+        }
+    }
+
+    static enum Grade {
+        A, B, C, D, F;
+    }
+
+
+}
+
+
